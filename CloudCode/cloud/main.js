288d464fd596568abda0c2047357ab6dd3f21e37
maxcode <maxcode@gmail.com>
2014-04-13 00:37:10 +0200

    split cloudcode

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -1,13 +1,40 @@
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
+
+// Add an activity.
 Parse.Cloud.define("AddNewActivity", function(request, response) {
+
+  var activities = [{
+      description:"Subside aanvraag",
+      accessor: 'vYbJ2wR1Vl',
+      flaggedByUser: false,
+      location: "M1hMxgFDmD",
+      src: 'FFzFpAjynb'
+    },{
+      description:"Adres verandering",
+      accessor: vYbJ2wR1Vl,
+      flaggedByUser: false,
+      location: 'M1hMxgFDmD',
+      src: 'W9TNikJ5il'
+    },{
+      description:"Rekening aanvraag",
+      accessor: vYbJ2wR1Vl,
+      flaggedByUser: false,
+      location: 'M1hMxgFDmD',
+      src: 'W9TNikJ5il'
+    },{
+      description:"ID-bewijs opnieuw aangevraagd",
+      accessor: yLb0IwLEk2,
+      flaggedByUser: false,
+      location: '9Ie05ILjzU',
+      src: 'W9TNikJ5il'
+  }];
+
   var Activity = Parse.Object.extend("Activity");
   var act = new Activity();
-   
-  act.set("description", "Uitschrijving bij gemeente");
-   
-  act.save(null, {
+
+  var number = int(request.params.number) % activities.length;
+
+  act.save(activities[number], {
     success: function(act) {
       response.success("Created new activity.");
     },
@@ -18,40 +45,5 @@ Parse.Cloud.define("AddNewActivity", function(request, response) {
 });
 
 
-//   Parse.Cloud.AfterSaveRequest()
 
-//   Parse.Push.send({
-//     channels: ["Activities"],
-//     expiration_time: new Date(2014, 4, 17),
-//     data: {
-//       alert: "Hello all!"
-//     }
-//   }, {
-//     success: function() {
-//       response.success("Hello world!");
-//     },
-//     error: function(error) {
-//       response.error("Bye world!");
-//     }
-//   });
-// });
-
-Parse.Cloud.afterSave('Activity', function(request) {
-  today = new Date();
-  Parse.Push.send({
-    channels: ["Activities"],
-    expiration_time: new Date().setDate(today.getDate()+7), // after 7 days
-    data: {
-      alert: request.object.get('description'),
-      object: request.object.get('objectId')
-    }
-  }, {
-    success: function() {
-      //response.success("Hello world!");
-    },
-    error: function(error) {
-      //response.error("Bye world!");
-    }
-  });
-});
 
