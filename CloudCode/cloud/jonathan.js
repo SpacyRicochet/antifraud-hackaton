5b0a1769faaa0644b60f862540edfa41e217e746
Bruno Scheele <bruno@noodlewerk.com>
2014-04-13 12:50:23 +0200

    Proper testdata in push scripts.

--- CloudCode/cloud/jonathan.js
+++ CloudCode/cloud/jonathan.js
@@ -3,37 +3,77 @@
 Parse.Cloud.define("AddNewActivity", function(request, response) {
   
   var activities = [{
-      description:"Subside aanvraag",
+      description:"DigiD",
+      accessorName: 'computer',
+      flaggedByUser: false,
+      city: 'Delft',
+      srcDatabase: 'DigiD',
+      location: new Parse.GeoPoint(52.016667,4.366667),
+      date: new Date(2014, 3, 4, 11, 20)
+    },{
+      description:"Ingeschreven Den Haag",
       accessorName: 'Gemeente Den Haag',
       flaggedByUser: false,
       city: 'Den Haag',
-      srcDatabase: 'GBA',
-      location: new Parse.GeoPoint(52.069694,4.300804),
-      date: new Date(2014, 2, 2)
+      srcDatabase: 'GBA,BAG',
+      location: new Parse.GeoPoint(52.083333,4.316667),
+      date: new Date(2014, 3, 7, 14, 00)
     },{
-      description:"Adres verandering",
-      accessorName: 'Gemeente Den Haag',
-      flaggedByUser: false,
-      city: 'Groningen',
-      srcDatabase: 'GBA',
-      location: new Parse.GeoPoint(52.069694,4.300804),
-      date: new Date(2014, 2, 24)
-    },{
-      description:"Rekening aanvraag",
+      description:"Uitgeschreven Delft",
       accessorName: 'Gemeente Den Haag',
       flaggedByUser:false,
-      city: 'Groningen',
-      srcDatabase: 'GBA',
-      location: new Parse.GeoPoint(52.069694,4.300804),
-      date: new Date(2014, 3, 1)
+      city: 'Den Haag',
+      srcDatabase: 'GBA,BAG',
+      location: new Parse.GeoPoint(52.083333,4.316667),
+      date: new Date(2014, 3, 7, 23, 00)
     },{
-      description:"ID-bewijs opnieuw aangevraagd",
+      description:"DigiD",
+      accessorName: 'computer',
+      flaggedByUser: false,
+      city: 'Den Haag',
+      srcDatabase: 'DigiD',
+      location: new Parse.GeoPoint(52.083333,4.316667),
+      date: new Date(2014, 3, 19, 14, 00)
+    },{
+      description:"Aangifte inbraak",
+      accessorName: 'Politie',
+      flaggedByUser: false,
+      city: 'Den Haag',
+      srcDatabase: 'GBA,BR,VR',
+      location: new Parse.GeoPoint(52.083333,4.316667),
+      date: new Date(2014, 3, 21, 21, 30)
+    },{
+      description:"Paspoort vermist",
+      accessorName: 'Politie',
+      flaggedByUser: false,
+      city: 'Den Haag',
+      srcDatabase: 'BR,VR',
+      location: new Parse.GeoPoint(52.083333,4.316667),
+      date: new Date(2014, 4, 10, 8, 10)
+    },{
+      description:"Ingeschreven Groningen",
       accessorName: 'Gemeente Groningen',
       flaggedByUser: false,
       city: 'Groningen',
-      srcDatabase: 'GBA',
-      location: new Parse.GeoPoint(53.221606,6.563988),
-      date: new Date(2014, 3, 5)
+      srcDatabase: 'Groningen',
+      location: new Parse.GeoPoint(52.216667,6.566667),
+      date: new Date(2014, 4, 10, 14, 00)
+    },{
+      description:"Bedrijf gestart",
+      accessorName: 'KvK',
+      flaggedByUser: false,
+      city: 'Groningen',
+      srcDatabase: 'KvK',
+      location: new Parse.GeoPoint(52.216667,6.566667),
+      date: new Date(2014, 4, 10, 15, 00)
+    },{
+      description:"Overleden",
+      accessorName: 'Universitair Medisch Centrum Groningen',
+      flaggedByUser: false,
+      city: 'Groningen',
+      srcDatabase: 'GBA,EPD',
+      location: new Parse.GeoPoint(52.216667,6.566667),
+      date: new Date(2014, 4, 12, 02, 42)
   }];
 
   var Activity = Parse.Object.extend("Activity");
